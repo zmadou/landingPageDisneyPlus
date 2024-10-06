036f81f94dbd115a146874c5592d81b57cf3ed47
zmadou <ferreira1337@hotmail.com>
2024-10-05 23:07:44 -0300

    menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const tabsContainer = document.querySelectorAll('[data-tab-id]');  
-    const questions = document.querySelectorAll('[data-faq-questions]') 
 
-    const heroSelection = document.querySelector('.hero')
+    const heroSelection = document.querySelector('header')
     const heightButton = heroSelection.clientHeight;
+    
     
 
     window.addEventListener('scroll', function() {
         const posicaoAtual = window.scrollY;
+        
 
+
+
+    
 
         if (posicaoAtual < heightButton) {
             ocultaElementosHeader()
@@ -32,41 +36,31 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
 
-    for (let i = 0; i < questions.length; i++) {
-        questions[i].addEventListener('click', abreOuFechaResposta);
-    }
 })  
 
-
-function abreOuFechaResposta(elemento) {
-    const classe = 'faq__questions__item--is-open';
-    const elementoPai = elemento.target.parentNode; 
-
-    elementoPai.classList.toggle(classe)
-}
 
 function removeBotaoAtivo () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('shows__tabs__button--is-active')
+        buttons[i].classList.remove('hero__shows__tabs__button--is-active')
     }
 }
 function escondeTodasAsAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('shows__list--is-active')
+        tabsContainer[i].classList.remove('hero__shows__list--is-active')
     }
 }
 
 
 function ocultaElementosHeader() {
     const header = document.querySelector('header')
-    header.classList.add('header--is-hidden')
+    header.classList.remove('header--is-transparent')
 }
 
 function exibeElementosHeader() {
     const header = document.querySelector('header')
-    header.classList.remove('header--is-hidden')
+    header.classList.add('header--is-transparent')
 }
